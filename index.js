f141f64ea80ef97f9e3e7da626190faf6ec737e1
mdyzhc8488 <riyasata62117@gmail.com>
2025-08-03 19:08:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ catch {
 const mineflayer = require('mineflayer');
 
 // ✅ CONFIG
-const IP = '191.96.231.2';       // Minecraft server IP
-const PORT = 10578;              // Server port
+const IP = 'Famous_SMP_1M.aternos.me';       // Minecraft server IP
+const PORT = 50705;              // Server port
 const WEB_PORT = process.env.PORT || 3000; // Fake port for Render/web host
 const PASSWORD = 'Mishra@123';
 
